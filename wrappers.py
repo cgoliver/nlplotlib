fa4f09fb44e0812c3e80990ba1cd452d98e40555
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2018-03-07 10:57:10 -0500

    temp plots and plot save

--- wrappers.py
+++ wrappers.py
@@ -2,22 +2,26 @@
 Plotting wrappers.
 """
 import uuid
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 
 def xx_make_line_plot(data=None):
     if not data:
         xs = np.random.randn(10)
-        fig, ax = plt.subplot()
+        fig, ax = plt.subplots()
         ax.plot(xs)
-        fig.savefig("static/plot.png", format="png")
+        fig.savefig("static/{}.png".format(uuid.uuid1()), format="png")
         
 def xx_make_scatter_plot(data=None):
     if not data:
         xs = np.random.randn(10)
         ys = np.random.randn(10)
         plt.scatter(ys)
-        plt.savefig("static/plot.png", format="png")
+        fig.savefig("static/{}.png".format(uuid.uuid1()), format="png")
 
 if __name__ == "__main__":
+    xx_make_line_plot()
     pass
