0d9e64c2a00be52b061bede4d2b1f4df1e33fae5
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2018-03-23 16:06:26 -0400

    embedding

---
+++ embedding.py
@@ -0,0 +1,21 @@
+from gensim.scripts.glove2word2vec import glove2word2vec
+from gensim.models import KeyedVectors
+
+
+
+def convert():
+    #convert to glove format
+    glove_input_file = 'Data/glove.6B/glove.6B.50d.txt'
+    word2vec_output_file = 'Data/glove.6B.50d.txt.word2vec'
+    glove2word2vec(glove_input_file, word2vec_output_file)
+
+def embed(modelpath='Data/glove.6B.50d.txt.word2vec'):
+    model = KeyedVectors.load_word2vec_format(modelpath, binary=False)
+    # calculate: (king - man) + woman = ?
+    result = model.most_similar(positive=['woman', 'king'], negative=['man'],\
+        topn=1)
+    print(model['roman'])
+    # print(result)
+
+if __name__ == "__main__":
+    embed()
